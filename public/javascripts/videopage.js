4baa1a921bb3471cf132120551bc299c980606ef
theonlypat <theonlypat@gmail.com>
2013-05-20 17:38:52 -0600

    Added a new background gif to get the plus sign to animate in video chat.

---
+++ public/javascripts/videopage.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {  
+  if ($('#streamsContainer').is(':empty')) {
+    $('#plussign').style.display="block";
+  } else {
+    $('#plussign').style.display="none";
+  };
+});
