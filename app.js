8b3f8ebc064be8771f8657a872fe1f120c1ac406
Dylan Turner <dylan.turner22@gmail.com>
2013-06-15 09:54:58 -0600

    replaced hardcoded api key with evn variable for obvious reasons... oops. Key is changed now

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var OpenTokLibrary = require('opentok');
 // *** OpenTok Constants for creating Session and Token values
 // ***
 var OTKEY = 24250552;
-var OTSECRET = "024025a7c2b92b69162c3af6147aabd7a46e6ad5";
+var OTSECRET = process.env.APIKEY;
 
 // ***
 // *** Setup when server first starts
