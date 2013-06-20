4433d0652dd04e4b04cfffd4aef68a49e38f2d03
Dylan Turner <dylan.turner22@gmail.com>
2013-06-20 16:42:33 -0500

    updated things

--- app.js
+++ app.js
@@ -10,16 +10,9 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+app.use(express.vhost('localhost', require('./doxy/app').app));
+app.use(express.vhost('utah.localhost', require('./utah/app').app));
 
-if ('development' == app.get('env')) {
-  app.use(express.vhost('localhost', require('./doxy/app').app));
-  app.use(express.vhost('utah.localhost', require('./utah/app').app));
-} else {
-  app.use(express.vhost('doxy', require('./doxy/app').app));
-  app.use(express.vhost('utah.doxy', require('./utah/app').app));
-}
-
-app.use(app.router);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
