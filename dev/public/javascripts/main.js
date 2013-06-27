87780634ab578a708810654cb3a528de8c7dc393
Dylan Turner <dylan.turner22@gmail.com>
2013-06-27 07:01:46 -0600

    trying to figure out where our problem is located

--- dev/public/javascripts/main.js
+++ dev/public/javascripts/main.js
@@ -1,53 +1,55 @@
-
-TB.setLogLevel(TB.DEBUG);
-// Initialize session, set up event listeners, and connect
-window.session = TB.initSession(sessionId);
-session.addEventListener('sessionConnected', sessionConnectedHandler);  
-session.addEventListener('streamCreated', streamCreatedHandler);
-session.connect(apiKey, token);
-
-TB.addEventListener("exception", exceptionHandler);
-
-function exceptionHandler(event) {
-    alert("Exception:" + event.title + ". " + event.message);
-}
-
-function sessionConnectedHandler(event) {
-  // Create publisher and start streaming into the session
-  var publisher = TB.initPublisher(apiKey, 'myPublisherDiv', {height: "135px", width: "180px"});
-  session.publish(publisher);
-
-  // Subscribe to streams that were in the session when we connected
-  subscribeToStreams(event.streams);
-
-}
-
-function streamCreatedHandler(event) {
-  // Subscribe to any new streams that are created
-  subscribeToStreams(event.streams);
-  $('#promptArrow').remove();
+(function () {
   
-}
+  TB.setLogLevel(TB.DEBUG);
+  // Initialize session, set up event listeners, and connect
+  var session = TB.initSession(sessionId);
+  session.addEventListener('sessionConnected', sessionConnectedHandler);  
+  session.addEventListener('streamCreated', streamCreatedHandler);
+  session.connect(apiKey, token);
 
-function subscribeToStreams(streams) {
-  for (var i = 0; i < streams.length; i++) {
-    // Make sure we don't subscribe to ourself
-    if (streams[i].connection.connectionId == session.connection.connectionId) {
-      return;
-    }
+  TB.addEventListener("exception", exceptionHandler);
 
-    // Create the div to put the subscriber element in to
-    var div = document.createElement('div');
-    div.setAttribute('id', 'stream' + streams[i].streamId);
-    var streamsContainer = document.getElementById('streamsContainer');
-    streamsContainer.appendChild(div);
-
-    var browserHeight = $(window).height();
-    var browserWidth = $(window).width();
-    // Subscribe to the stream
-    session.subscribe(streams[i], div.id, {height: browserHeight + "px", width: browserWidth + "px"});
+  function exceptionHandler(event) {
+      alert("Exception:" + event.title + ". " + event.message);
   }
-}
+
+  function sessionConnectedHandler(event) {
+    // Create publisher and start streaming into the session
+    var publisher = TB.initPublisher(apiKey, 'myPublisherDiv', {height: "135px", width: "180px"});
+    session.publish(publisher);
+
+    // Subscribe to streams that were in the session when we connected
+    subscribeToStreams(event.streams);
+
+  }
+
+  function streamCreatedHandler(event) {
+    // Subscribe to any new streams that are created
+    subscribeToStreams(event.streams);
+    $('#promptArrow').remove();
+    
+  }
+
+  function subscribeToStreams(streams) {
+    for (var i = 0; i < streams.length; i++) {
+      // Make sure we don't subscribe to ourself
+      if (streams[i].connection.connectionId == session.connection.connectionId) {
+        return;
+      }
+
+      // Create the div to put the subscriber element in to
+      var div = document.createElement('div');
+      div.setAttribute('id', 'stream' + streams[i].streamId);
+      var streamsContainer = document.getElementById('streamsContainer');
+      streamsContainer.appendChild(div);
+
+      var browserHeight = $(window).height();
+      var browserWidth = $(window).width();
+      // Subscribe to the stream
+      session.subscribe(streams[i], div.id, {height: "450px", width: "600px"});
+    }
+  }
 
 
 
+})();
