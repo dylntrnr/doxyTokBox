6d4af9910a43f04a76af5e7be444314839da6527
Dylan Turner <dylan.turner22@gmail.com>
2013-06-29 10:08:57 -0600

    upadted app.js, javascript folders, meeting view - I updated the email link and deleted one jquery img class thing that wasn't suppose to be there but maybe influenced the design, and updated the email links in the confirmed view

--- utah/public/javascripts/main.js
+++ utah/public/javascripts/main.js
@@ -1,19 +1,19 @@
+(function () {
 
+  TB.setLogLevel(TB.DEBUG);
+  // Initialize session, set up event listeners, and connect
+  var session = TB.initSession(sessionId);
+  session.addEventListener('sessionConnected', sessionConnectedHandler);
+  session.addEventListener('streamCreated', streamCreatedHandler);
+  session.connect(apiKey, token);
 
-TB.setLogLevel(TB.DEBUG);
-// Initialize session, set up event listeners, and connect
-window.session = TB.initSession(sessionId);
-session.addEventListener('sessionConnected', sessionConnectedHandler);  
-session.addEventListener('streamCreated', streamCreatedHandler);
-session.connect(apiKey, token);
+  TB.addEventListener("exception", exceptionHandler);
 
-TB.addEventListener("exception", exceptionHandler);
-
-function exceptionHandler(event) {
+  function exceptionHandler(event) {
     alert("Exception:" + event.title + ". " + event.message);
-}
+  }
 
-function sessionConnectedHandler(event) {
+  function sessionConnectedHandler(event) {
   // Create publisher and start streaming into the session
   var publisher = TB.initPublisher(apiKey, 'myPublisherDiv', {height: "135px", width: "180px"});
   session.publish(publisher);
@@ -21,16 +21,16 @@ function sessionConnectedHandler(event) {
   // Subscribe to streams that were in the session when we connected
   subscribeToStreams(event.streams);
 
-}
+  }
 
-function streamCreatedHandler(event) {
+  function streamCreatedHandler(event) {
   // Subscribe to any new streams that are created
   subscribeToStreams(event.streams);
   $('#promptArrow').remove();
-  
-}
 
-function subscribeToStreams(streams) {
+  }
+
+  function subscribeToStreams(streams) {
   for (var i = 0; i < streams.length; i++) {
     // Make sure we don't subscribe to ourself
     if (streams[i].connection.connectionId == session.connection.connectionId) {
@@ -44,10 +44,8 @@ function subscribeToStreams(streams) {
     streamsContainer.appendChild(div);
 
     // Subscribe to the stream
-    session.subscribe(streams[i], div.id, {height: "450px", width: "600px"});
-  //   var plussign = document.getElementById("plussign");
-  //   plussign.remove();
+    session.subscribe(streams[i], div.id, { height: "100%", width: "100%"});
   }
-}
+  }
 
-
+})();
